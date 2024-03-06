34da4a03d306ead92e4c2602c147c8de8ba0a102
maxrchung <maxrchung@gmail.com>
2024-03-06 10:59:58 -0800

    some start

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -1,9 +1,63 @@
-import { Box } from "@chakra-ui/react";
+import { Button, Grid, Img, chakra } from "@chakra-ui/react";
+import { useState } from "react";
 
 export default function Index() {
   return (
-    <Box bg="tomato" w="100%" p={4} color="white">
-      Hello World!
-    </Box>
+    <Grid
+      templateColumns="repeat(4, 1fr)"
+      autoRows="auto"
+      w="container.sm"
+      p="8"
+      flexDirection="column-reverse"
+    >
+      <StepButton />
+      <StepButton />
+      <StepButton />
+      <StepButton />
+
+      <StepButton />
+      <StepButton />
+      <StepButton />
+      <StepButton />
+
+      <StepButton />
+      <StepButton />
+      <StepButton />
+      <StepButton />
+
+      <StepButton />
+      <StepButton />
+      <StepButton />
+      <StepButton />
+
+      <StepButton />
+      <StepButton />
+      <StepButton />
+      <StepButton />
+    </Grid>
   );
 }
+
+const StepButton = () => {
+  const [hasNote, setHasNote] = useState(true);
+
+  return (
+    <chakra.button
+      onClick={() => setHasNote(!hasNote)}
+      position="relative"
+      bg="gray.100"
+      display="flex"
+      borderTop="1px solid black"
+      justifyContent="center"
+      h="16"
+    >
+      <Img
+        src="https://maxrchung.com/the-boy.jpg"
+        position="absolute"
+        width="100px"
+        top={0}
+        transform="translateY(-50%)"
+      />
+    </chakra.button>
+  );
+};
