21fa9612fba2f31a36d73cf278c42d7b5b5ac6de
maxrchung <maxrchung@gmail.com>
2024-03-14 15:35:33 -0700

    add google sign in button

---
+++ app/components/GoogleSignInButton.tsx
@@ -0,0 +1,11 @@
+import { Button, Icon } from "@chakra-ui/react";
+import GoogleIcon from "./GoogleIcon";
+
+export default function GoogleSignInButton() {
+  return (
+    <Button display="flex" gap="2" alignItems="center">
+      <GoogleIcon />
+      Sign in with Google
+    </Button>
+  );
+}
